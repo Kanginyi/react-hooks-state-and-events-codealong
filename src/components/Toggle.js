0b2349eab8745cbde19f135ff9ebf58287cf1ffa
Eric Yi <erickanginyi@gmail.com>
2021-10-01 13:32:39 -0400

    bingo bango

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [clickButton, setClickButton] = useState(false);
+
+  function handleButton() {
+    setClickButton(click => !click);
+  }
+
+  const color = clickButton ? {background: "red"} : {background: "white"};
+
+  return <button style={color} onClick={handleButton}>{clickButton ? "ON" : "OFF"}</button>;
 }
 
 export default Toggle;
